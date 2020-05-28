7584bcb126867c050a9f2f2e3e61f0b5057d3ca2
jedrichards <jedrichards@gmail.com>
2020-05-28 14:14:44 +0100

    Implement

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,45 @@
-import { Assistant } from '@sketch-hq/sketch-assistant-types'
+import {
+  AssistantPackageExport,
+  AssistantConfig,
+  ViolationSeverity,
+} from '@sketch-hq/sketch-assistant-types'
+import CoreAssistant from '@sketch-hq/sketch-core-assistant'
 
-import { helloWorldRule } from './rules/hello-world'
-
-const assistant: Assistant = async () => {
-  return {
-    name: 'sketch-assistant-template',
-    rules: [helloWorldRule],
-    config: {
-      rules: {
-        [helloWorldRule.name]: { active: true },
-      },
+export const config: AssistantConfig = {
+  rules: {
+    '@sketch-hq/sketch-core-assistant/name-pattern-pages': {
+      active: true,
+      forbidden: [],
+      allowed: ['^(?![ a-zA-Z0-9,.-_;!"#$%&/()@Û£><|\\¼»`«]).'],
+      ruleTitle: 'Page names should start with an emoji',
+      severity: ViolationSeverity.info,
     },
-  }
+    '@sketch-hq/sketch-core-assistant/name-pattern-artboards': {
+      active: true,
+      forbidden: [],
+      allowed: ['^(\\d+\\.?)+ .*'],
+      ruleTitle: 'Artboard names should start with numbers',
+      severity: ViolationSeverity.warn,
+    },
+    '@sketch-hq/sketch-core-assistant/name-pattern-groups': {
+      active: true,
+      allowed: [],
+      forbidden: ['^Group$', 'Group Copy'],
+      ruleTitle: 'Group names should not be default',
+      severity: ViolationSeverity.error,
+    },
+  },
 }
+
+const assistant: AssistantPackageExport = [
+  CoreAssistant,
+  async () => {
+    return {
+      name: 'severity-triggers',
+      rules: [],
+      config,
+    }
+  },
+]
 
 export default assistant
